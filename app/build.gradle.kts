cb640c532af1e6291c4cc25819690206b33bb9ef
matiiguzman5 <Matiiguzman03@gmail.com>
2024-09-06 23:39:53 -0300

    Se le agrego el logo a la aplicacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.App.lectormedidorelec"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
